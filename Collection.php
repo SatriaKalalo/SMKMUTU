3a63e735c43a30851e93f4f7bdb132a9c7e5a595
SatriaKalalo <112679772+SatriaKalalo@users.noreply.github.com>
2024-01-31 07:35:13 +0700

    Add files via upload

---
+++ Collection.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="Collection.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="FContainer">
+        <nav class="wrapper">
+            <div class="brand">
+                <div class="Nama1">Smkmutuckp</div>
+                <div class="Nama2">.Id</div>
+            </div>
+            <ul class="navigation">
+                <li><a href="navbar.php">Home</a></li>
+                <li><a href="Collection.php" class="active">Collection</a></li>
+                <li><a href="major.php">Major</a></li>
+                <li><a href="event.php">Event</a></li>
+                <li><a href="registration.php">Purchase</a></li>
+                <li><a class="teks3" href="contact.php">Help</a></li>
+                <li><img id="profile-picture" src="logo_mutu_png_transparant-removebg-preview-1.png" alt="Foto Profile"></li>
+            </ul>
+        </nav>
+        <div class="body1">
+            <div class="container">
+                <div class="kartu" style="--clr:#0f0">
+                    <div class="imgBx">
+                        <img src="https://www.smkmutucikampek.sch.id/wp-content/uploads/2020/04/RPL-750x410.jpg" alt="">
+                    </div>
+                    <div class="konten">
+                        <h2>PPLG </h2>
+                        <a href="Collection/TahunRPL.php" style="background:#0f0">Lihat ></a>
+                    </div>
+                </div>
+                <div class="kartu" style="--clr:#ff0">
+                    <div class="imgBx">
+                        <img src="https://www.smkmutucikampek.sch.id/wp-content/uploads/2020/04/72714018_2387386701390092_8469427580724838400_o-750x410.jpg" alt="">
+                    </div>
+                    <div class="konten">
+                        <h2>TBSM</h2>
+                        <a href="Collection/TahunTBSM.php" style="background:#ff0">Lihat ></a>
+                    </div>
+                </div>
+                <div class="kartu" style="--clr:#008000">
+                    <div class="imgBx">
+                        <img src="https://www.smkmutucikampek.sch.id/wp-content/uploads/2020/04/TKJ-750x410.jpg" alt="">
+                    </div>
+                    <div class="konten">
+                        <h2>TKJ</h2>
+                        <a href="Collection/TahunTKJ.php" style="background:#008000">Lihat ></a>
+                    </div>
+                </div>
+                <div class="kartu" style="--clr:#036494">
+                    <div class="imgBx">
+                        <img src="https://www.smkmutucikampek.sch.id/wp-content/uploads/2020/04/TKR-750x410.jpg" alt="">
+                    </div>
+                    <div class="konten">
+                        <h2>TKR</h2>
+                        <a href="Collection/TahunTKR.php" style="background:#036494">Lihat ></a>
+                    </div>
+                </div>
+                <div class="kartu" style="--clr:#964b00">
+                    <div class="imgBx">
+                        <img src="https://www.smkmutucikampek.sch.id/wp-content/uploads/2020/04/OTKP1.1-750x410.jpg" alt="">
+                    </div>
+                    <div class="konten">
+                        <h2>OTKP</h2>
+                        <a href="Collection/TahunOTKP.php" style="background:#964b00">Lihat ></a>
+                    </div>
+                </div>
+                <div class="kartu" style="--clr:#ff0000">
+                    <div class="imgBx">
+                        <img src="https://www.smkmutucikampek.sch.id/wp-content/uploads/2020/04/TPMI1.1-750x410.jpg" alt="">
+                    </div>
+                    <div class="konten">
+                        <h2>TMI</h2>
+                        <a href="Collection/TahunTMI.php" style="background:#ff0000">Lihat ></a>
+                    </div>
+                </div>
+                <div class="kartu" style="--clr:#228B22">
+                    <div class="imgBx">
+                        <img src="//www.smkmutucikampek.sch.id/wp-content/uploads/2020/01/82248481_3386497748088061_8471112535049764864_o.jpg" alt="">
+                    </div>
+                    <div class="konten">
+                        <h2>GURU</h2>
+                        <a href="Collection/TahunGURU.php" style="background:#228B22">Lihat ></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div>
+          <div id=©1>
+            <p>© 2023 ALL RIGHTS RESERVED</p>
+            <p>POWERED BY SMK MUTU CIKAMPEK</p>
+          </div>
+        </div>
+</body>
+  <script>
+    document.getElementById('profile-picture').addEventListener('click', function() {
+    // Redirect ke halaman profil, ganti URL berikut dengan URL halaman profil yang diinginkan
+    window.location.href = 'Profile.php';
+});
+  </script>
+</html>
